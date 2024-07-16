078122883c83a3ae6cf74a90c52a79b0be8a6e10
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2024-07-17 01:17:28 +0800

    Added Frontend Code with State Management and Icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='bg-red-200 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={ <Home/> }></Route>
+      <Route path='/books/create' element={ <CreateBook/> }></Route>
+      <Route path='/books/delete/:id' element={ <DeleteBook/> }></Route>
+      <Route path='/books/edit/:id' element={ <EditBook/> }></Route>
+      <Route path='/books' element={ <ShowBook/> }></Route>
+    </Routes>
   )
 }
 
